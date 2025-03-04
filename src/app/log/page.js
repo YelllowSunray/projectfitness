3ce3f4a4298947f323e6f5b10839a6a3ba01ea4b
YelllowSunray <152189169+YelllowSunray@users.noreply.github.com>
2025-03-04 14:38:28 +0100

    Your commit message

--- src/app/log/page.js
+++ src/app/log/page.js
@@ -10,56 +10,243 @@ const WorkoutLog = () => {
         {
           time: "11:00",
           exercises: [
-            "4x6 benchpress 30kg",
-            "2x6 tricep extension",
-            "1x5 pushups",
-            "2x5 bicep curl"
+            "4x6 Bench Press 30kg",
+            "2x6 Tricep Extension",
+            "1x5 Push-ups Bodyweight",
+            "2x5 Bicep Curl"
           ]
         },
         {
           time: "14:30",
           exercises: [
-            "3x8 squat 16kg",
-            "3x8 Leg press 40kg",
-            "3x8 R/L lunges 8kg - support",
+            "3x8 Squat 16kg",
+            "3x8 Leg Press 40kg",
+            "3x8 Lunges (R/L) 8kg",
             "1x8 Squats 20kg"
           ]
         },
         {
           time: "16:00",
           exercises: [
-            "3x3 deadlift 40kg, pr 60kg",
-            "2x6 leg lifts 0kg"
+            "3x3 Deadlift 40kg, PR 60kg",
+            "2x6 Leg Lifts 60kg"
           ]
         },
         {
           time: "18:00",
           exercises: [
-            "3x8 pushups"
+            "3x8 Push-ups Bodyweight"
           ]
         }
       ]
     },
-    // ... (previous entries remain the same)
+    {
+      date: "18/02/2025",
+      sessions: [
+        {
+          time: "11:00",
+          exercises: [
+            "4x6 Bench Press 25kg"
+          ]
+        },
+        {
+          time: "16:00",
+          exercises: [
+            "3x8 Lat Pulldown 70kg",
+            "3x7 Row 60kg"
+          ]
+        },
+        {
+          time: "18:00",
+          exercises: [
+            "2x4min Running",
+            "2x7 Barbell Row 20kg",
+            "2x7 Dumbbell Row 8kg"
+          ]
+        }
+      ]
+    },
+    {
+      date: "19/02/2025",
+      sessions: [
+        {
+          time: "11:30",
+          exercises: [
+            "2x7 Bench Press 30kg",
+            "2x7 Incline Bench Press 10kg"
+          ]
+        },
+        {
+          time: "14:30",
+          exercises: [
+            "1x3min Running",
+            "3x5 Bench Press 30kg",
+            "4x7 Incline Bench Press 16kg",
+            "2x8 Lateral Raises 8kg"
+          ]
+        },
+        {
+          time: "16:00",
+          exercises: [
+            "4x8 Incline Dumbbell Press 20kg"
+          ]
+        },
+        {
+          time: "19:00",
+          exercises: [
+            "3x8 Push-ups Bodyweight"
+          ]
+        }
+      ]
+    },
+    {
+      date: "20/02/2025",
+      sessions: [
+        {
+          time: "13:00",
+          exercises: [
+            "3x6 Dumbbell Press 20kg",
+            "3x6 Bench Press 30kg",
+            "3x6 Bicep Curl 12kg"
+          ]
+        }
+      ]
+    },
+    {
+      date: "21/02/2025",
+      sessions: [
+        {
+          time: "14:30",
+          exercises: [
+            "1x30min Boxing"
+          ]
+        },
+        {
+          time: "16:00",
+          exercises: [
+            "5x8 Squat 30kg"
+          ]
+        }
+      ]
+    },
+    {
+      date: "22/02/2025",
+      sessions: [
+        {
+          time: "13:30",
+          exercises: [
+            "4x6 Deadlift 60kg",
+            "3x8 Dip/Pull-ups (Assisted) 20kg",
+            "1x8 Tricep Extension",
+            "4x8 Bayesian Curl 5kg",
+            "3x8 Chest Fly 15kg"
+          ]
+        }
+      ]
+    },
+    {
+      date: "24/02/2025",
+      sessions: [
+        {
+          time: "11:00",
+          exercises: [
+            "1x30min Boxing",
+            "4x3 Bench Press 25kg",
+            "2x4 Dumbbell Press 20kg"
+          ]
+        },
+        {
+          time: "14:30",
+          exercises: [
+            "6x6 Bench Press 30kg"
+          ]
+        },
+        {
+          time: "16:00",
+          exercises: [
+            "2x8 Lunges (R/L) 8kg",
+            "2x8 Box Jump"
+          ]
+        }
+      ]
+    },
+    {
+      date: "25/02/2025",
+      sessions: [
+        {
+          time: "11:00",
+          exercises: [
+            "3x6 Deadlift 50kg",
+            "1x16 Standing Cable Hamstring (L/R)"
+          ]
+        },
+        {
+          time: "16:30",
+          exercises: [
+            "3x8 Squat 30kg"
+          ]
+        }
+      ]
+    },
     {
       date: "26/02/2025",
       sessions: [
         {
           time: "11:15",
           exercises: [
-            "3x6 bench press 35kg"
+            "3x6 Bench Press 35kg"
           ]
         },
         {
           time: "14:45",
           exercises: [
-            "1x6 chest flies 10kg"
+            "1x6 Chest Fly 10kg"
           ]
         },
         {
           time: "16:00",
           exercises: [
-            "5x3min running"
+            "5x3min Running"
+          ]
+        }
+      ]
+    },
+    {
+      date: "27/02/2025",
+      sessions: [
+        {
+          time: "18:00",
+          exercises: [
+            "5x8 Push-ups Bodyweight"
+          ]
+        }
+      ]
+    },
+    {
+      date: "28/02/2025",
+      sessions: [
+        {
+          time: "11:00",
+          exercises: [
+            "3x8 Squat 20kg",
+            "2x7 Bent Over Row 14kg"
+          ]
+        },
+        {
+          time: "14:45",
+          exercises: [
+            "2x8 Push-ups Bodyweight"
+          ]
+        }
+      ]
+    },
+    {
+      date: "29/02/2025",
+      sessions: [
+        {
+          time: "17:30",
+          exercises: [
+            "1x30min Beginner Calisthenics Bodyweight"
           ]
         }
       ]
